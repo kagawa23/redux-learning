348291610daf3406c55c52c15b6f10bb2387f070
Zhaochen <zhaochen01@juran.com.cn>
2018-06-28 17:47:58 +0800

    saga middleware

--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import App from './app';
 import { createStore, applyMiddleware, compose } from 'redux';
-import { counter,addGUN, removeGUN, addGunAsync } from './index.redux'
+import { counter, rootSaga } from './index.redux';
+import createSagaMiddleware from 'redux-saga'
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 
 const reduxDevTool = window.devToolsExtension ? window.devToolsExtension:()=>{};
-const store = createStore(counter, compose(applyMiddleware(thunk), reduxDevTool()));
+// const store = createStore(counter, compose(applyMiddleware(thunk), reduxDevTool()));
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(counter, compose(applyMiddleware(sagaMiddleware), reduxDevTool()));
+//const store = createStore(counter, applyMiddleware(sagaMiddleware));
+
+sagaMiddleware.run(rootSaga)
 
 ReactDom.render(
 <Provider store={store}>
--- src/index.redux.js
+++ src/index.redux.js
@@ -1,6 +1,10 @@
+import {delay} from 'redux-saga'
+import {call, put, takeEvery} from 'redux-saga/effects';
+
 // reducer
 const ADD_GUN = "家机关枪";
 const REMOVE_GUN = "减机关枪";
+const INCREMENT_ASYNC = "再等一会";
 
 export function counter(state=0,action){
     switch(action.type){
@@ -9,7 +13,7 @@ export function counter(state=0,action){
         case REMOVE_GUN:
             return state -1;
         default:
-        return 10
+            return state;
     } 
 }
 
@@ -23,10 +27,33 @@ export function removeGUN(){
     return {type:REMOVE_GUN};
 }
 
-export function addGunAsync() {
-    return (dispatch) => {
-        setTimeout(() => {
-            dispatch(addGUN());
-        }, 2000);
-    }
+export function addGunAsync(){
+    return {type:INCREMENT_ASYNC};
+}
+
+// export function addGUNAsyncA(){
+//     retrun {type:INCREMENT_ASYNC};
+// }
+
+// export function addGunAsync() {
+//     return (dispatch) => {
+//         setTimeout(() => {
+//             dispatch(addGUN());
+//         }, 2000);
+//     }
+// }
+
+function* addGunAsyncAction() {
+    // return (dispatch) => {
+    //     setTimeout(() => {
+    //         dispatch(addGUN());
+    //     }, 2000);
+    //}
+    yield delay(2000);
+    yield put(addGUN());
+   // yield put(removeGUN());
+}
+
+export function* rootSaga() {
+    yield takeEvery(INCREMENT_ASYNC, addGunAsyncAction)
 }
